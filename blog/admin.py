58964e1e25f1ff0ade6a1063f5ccbd732dda3a23
rgc <2020956572@qq.com>
2018-09-28 13:56:21 +0800

    tet

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from blog.models import Blog
+
+
+@admin.register(Blog)
+class PersonAdmin(admin.ModelAdmin):
+	# http://www.liujiangblog.com/course/django/158  自定制Admin
+	prepopulated_fields = {'url': ('name',)}
+	list_filter = ('name', 'age', 'bool')
